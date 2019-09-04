271ad432685bd8278d94d953b53ac6dbedc78768
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-09-04 10:08:32 +0530

    ANDROID: sched: WALT: Refactor cumulative runnable average fixup
    When a runnable task window is rolled over, it's CPU cumulative
    runnable average needs to be updated accordingly. Currently this
    is handled in a common function for all scheduling classes. However
    when CFS_BANDWIDTH is enabled, fair scheduling class needs special
    handling. As a preparation step, add a callback in sched_class
    for updating the cumulative runnable average. The subsequent patches
    make use of this callback and implement a different method for
    fair scheduling class.
    
    Bug: 139071966
    Change-Id: Iffa88ab89254932177b1ea4eeaf12ec305c1c9c6
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    Signed-off-by: Todd Kjos <tkjos@google.com>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -3674,6 +3674,9 @@ const struct sched_class rt_sched_class = {
 #ifdef CONFIG_RT_GROUP_SCHED
 	.task_change_group	= task_change_group_rt,
 #endif
+#ifdef CONFIG_SCHED_WALT
+	.fixup_cumulative_runnable_avg = walt_fixup_cumulative_runnable_avg,
+#endif
 };
 
 #ifdef CONFIG_RT_GROUP_SCHED

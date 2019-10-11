52d87793c8c5d6bec3b8e2b913be877c0cf61269
Jia He <justin.he@arm.com>
2019-10-11 22:09:36 +0800

    arm64: cpufeature: introduce helper cpu_has_hw_af()
    We unconditionally set the HW_AFDBM capability and only enable it on
    CPUs which really have the feature. But sometimes we need to know
    whether this cpu has the capability of HW AF. So decouple AF from
    DBM by a new helper cpu_has_hw_af().
    
    If later we noticed a potential performance issue on this path, we can
    turn it into a static label as with other CPU features.
    
    Signed-off-by: Jia He <justin.he@arm.com>
    Suggested-by: Suzuki Poulose <Suzuki.Poulose@arm.com>
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -284,6 +284,19 @@ void arm64_set_ssbd_mitigation(bool state);
 static inline void arm64_set_ssbd_mitigation(bool state) {}
 #endif
 
+/* Check whether hardware update of the Access flag is supported */
+static inline bool cpu_has_hw_af(void)
+{
+	u64 mmfr1;
+
+	if (!IS_ENABLED(CONFIG_ARM64_HW_AFDBM))
+		return false;
+
+	mmfr1 = read_cpuid(ID_AA64MMFR1_EL1);
+	return cpuid_feature_extract_unsigned_field(mmfr1,
+						ID_AA64MMFR1_HADBS_SHIFT);
+}
+
 #endif /* __ASSEMBLY__ */
 
 #endif

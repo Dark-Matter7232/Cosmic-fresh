eed7fcec5b7fa4ba7697a11a2801313a842c5da7
Alex Winkowski <dereference23@outlook.com>
2021-03-16 14:06:17 +0000

    mm: Set swappiness for CONFIG_INCREASE_MAXIMUM_SWAPPINESS=y case
    also set default swappiness to 100
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -176,7 +176,11 @@ static inline bool is_kanond(struct scan_control *sc)
 /*
  * From 0 .. 100.  Higher means more swappy.
  */
-int vm_swappiness = 60;
+#ifndef CONFIG_INCREASE_MAXIMUM_SWAPPINESS
+int vm_swappiness = 100;
+#else
+int vm_swappiness = 190;
+#endif
 /*
  * The total number of pages which are beyond the high watermark within all
  * zones.

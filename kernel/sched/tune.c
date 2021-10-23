404149e41d11c28b05d4df315d76eecd44c2ff9b
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2021-10-24 00:32:01 +0530

    set background level to -10

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -890,7 +890,7 @@ static void write_default_values(struct cgroup_subsys_state *css)
 {
 	static struct st_data st_targets[] = {
 		{ "audio-app",	0, 0, 0, 0 },
-		{ "background",	-25, 0, 0, 0 },
+		{ "background",	-10, 0, 0, 0 },
 		{ "foreground",	1, 1, 0, 0 },
 		{ "rt",		0, 0, 0, 0 },
 		{ "top-app",	2, 1, 0, 0 },          

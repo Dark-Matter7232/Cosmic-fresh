596776d67ea2fd9352b66d0c4609473aab200123
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-10-31 20:42:33 +0530

    treewide: Silence samsung's retarded dmesg spam
    Change-Id: Ic6b5db53eda32d36130d1345398ca2a8c1968a4c

--- drivers/sensorhub/ssp.h
+++ drivers/sensorhub/ssp.h
@@ -25,17 +25,15 @@
 #include "ssp_type_define.h"
 #include "ssp_platform.h"
 #include "factory/ssp_factory.h"
-#define ssp_dbg(fmt, ...)
-
-#define ssp_info(fmt, ...)
+#define ssp_dbg(fmt, ...) ((void)0)
+#define ssp_info(fmt, ...) ((void)0)
 
 #define ssp_err(fmt, ...) do { \
 	pr_err("[SSP] " fmt "\n", ##__VA_ARGS__); \
 	} while (0)
 
-#define ssp_dbgf(fmt, ...)
-
-#define ssp_infof(fmt, ...)
+#define ssp_dbgf(fmt, ...) ((void)0)
+#define ssp_infof(fmt, ...) ((void)0)
 
 #define ssp_errf(fmt, ...) do { \
 	pr_err("[SSP] %20s(%4d): " fmt "\n", __func__, __LINE__, ##__VA_ARGS__); \

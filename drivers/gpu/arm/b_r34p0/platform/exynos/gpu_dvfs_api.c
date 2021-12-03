9135a28f1d1d79deb9ea32912f7ccaa7a10d0007
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-12-03 22:01:03 +0530

    ARM64: configs: Compile Biforst R34P0 ARM MALI Midguard GPU drivers
    Change-Id: I5663ee1e3d762359a6a72eec25f7a8dcfca11721
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

--- drivers/gpu/arm/b_r34p0/platform/exynos/gpu_dvfs_api.c
+++ drivers/gpu/arm/b_r34p0/platform/exynos/gpu_dvfs_api.c
@@ -27,7 +27,7 @@
 #include "gpu_dvfs_handler.h"
 #include "gpu_dvfs_governor.h"
 
-#ifdef CONFIG_EXYNOS_BTS
+#ifdef CONFIG_EXYNOS9630_BTS
 #include <soc/samsung/bts.h>
 #endif
 
@@ -155,9 +155,9 @@ int gpu_set_target_clk_vol(int clk, bool pending_is_allowed)
 #define BS_G3D_PERFORMANCE BS_G3D_PEFORMANCE
 #endif
 
-#ifdef CONFIG_EXYNOS_BTS
 	/* MALI_SEC_INTEGRATION : for EXYNOS_BTS */
 	if (platform->gpu_bts_support) {
+#ifdef CONFIG_EXYNOS9630_BTS
 		if (target_clk >= platform->mo_min_clock && !platform->is_set_bts) {
 			bts_add_scenario(platform->bts_scen_idx);
 			platform->is_set_bts = 1;
@@ -165,8 +165,13 @@ int gpu_set_target_clk_vol(int clk, bool pending_is_allowed)
 			bts_del_scenario(platform->bts_scen_idx);
 			platform->is_set_bts = 0;
 		}
-	}
+#else
+		if (target_clk >= platform->mo_min_clock)
+			bts_update_scen(BS_G3D_PERFORMANCE, 1); /* GPU IDQ : 0 (max token) */
+		else
+			bts_update_scen(BS_G3D_PERFORMANCE, 0); /* GPU IDQ : 0x3 (default 12ea) */
 #endif
+	}
 
 	mutex_unlock(&platform->gpu_clock_lock);
 

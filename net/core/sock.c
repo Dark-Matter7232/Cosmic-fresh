9d023ae7907aa40c51e84c67df6185a7314054b9
UtsavBalar1231 <utsavbalar1231@gmail.com>
2021-12-05 11:57:34 +0530

    net: Allow disabling samsung NCM feature find net/ -type f -exec sed -i 's/\/\/\ SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA {/#ifdef\ CONFIG_KNOX_NCM/g' {} \; find net/ -type f -exec sed -i 's/\/\/\ SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA }/#endif/g' {} \; find include/ -type f -exec sed -i 's/\/\/\ SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA {/#ifdef\ CONFIG_KNOX_NCM/g' {} \; find include/ -type f -exec sed -i 's/\/\/\ SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA }/#endif/g' {} \;

--- net/core/sock.c
+++ net/core/sock.c
@@ -141,11 +141,11 @@
 
 #include <net/tcp.h>
 #include <net/busy_poll.h>
-// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA {
+#ifdef CONFIG_KNOX_NCM
 #include <linux/sched.h>
 #include <linux/pid.h>
 #include <net/ncm.h>
-// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA }
+#endif
 
 static DEFINE_MUTEX(proto_list_mutex);
 static LIST_HEAD(proto_list);
@@ -661,7 +661,7 @@ out:
 	return ret;
 }
 
-// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA {
+#ifdef CONFIG_KNOX_NCM
 /** The function sets the domain name associated with the socket. **/
 static int sock_set_domain_name(struct sock *sk, char __user *optval,
 				int optlen)
@@ -748,7 +748,7 @@ out:
 	return ret;
 }
 
-// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA }
+#endif
 static inline void sock_valbool_flag(struct sock *sk, int bit, int valbool)
 {
 	if (valbool)
@@ -797,14 +797,14 @@ int sock_setsockopt(struct socket *sock, int level, int optname,
 	if (optname == SO_BINDTODEVICE)
 		return sock_setbindtodevice(sk, optval, optlen);
 
-	// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA {
+	#ifdef CONFIG_KNOX_NCM
 	if (optname == SO_SET_DOMAIN_NAME)
 		return sock_set_domain_name(sk, optval, optlen);
 	if (optname == SO_SET_DNS_UID)
 		return sock_set_dns_uid(sk, optval, optlen);
 	if (optname == SO_SET_DNS_PID)
 		return sock_set_dns_pid(sk, optval, optlen);
-	// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA }
+	#endif
 	if (optlen < sizeof(int))
 		return -EINVAL;
 
@@ -1623,7 +1623,7 @@ struct sock *sk_alloc(struct net *net, int family, gfp_t priority,
 {
 	struct sock *sk;
 
-	// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA {
+	#ifdef CONFIG_KNOX_NCM
 	struct pid *pid_struct = NULL;
 	struct task_struct *task = NULL;
 	int process_returnValue = -1;
@@ -1632,11 +1632,11 @@ struct sock *sk_alloc(struct net *net, int family, gfp_t priority,
 	struct task_struct *parent_task = NULL;
 	int parent_returnValue = -1;
 	char full_parent_process_name[PROCESS_NAME_LEN_NAP] = {0};
-	// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA }
+	#endif
 	sk = sk_prot_alloc(prot, priority | __GFP_ZERO, family);
 	if (sk) {
 		sk->sk_family = family;
-		// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA {
+		#ifdef CONFIG_KNOX_NCM
 		/* assign values to members of sock structure when npa flag is present */
 		sk->knox_uid = current->cred->uid.val;
 		sk->knox_pid = current->tgid;
@@ -1678,7 +1678,7 @@ struct sock *sk_alloc(struct net *net, int family, gfp_t priority,
 				}
 			}
 		}
-		// SEC_PRODUCT_FEATURE_KNOX_SUPPORT_NPA }
+		#endif
 		/*
 		 * See comment in struct sock definition to understand
 		 * why we need sk_prot_creator -acme

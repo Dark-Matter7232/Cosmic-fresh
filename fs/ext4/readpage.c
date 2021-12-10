0bc4f68b5e1bc01740bf6910bf3a13083dce2d5f
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2021-12-10 15:54:18 +0530

    fs: ext4-f2fs: Remove remaining traces of Android & f2fs io trace code * this commit is follow-up to 2f48e3aa20 796a3404d4
    Signed-off-by: Dark-Matter7232 <kerneldeveloper7232@gmail.com>

--- fs/ext4/readpage.c
+++ fs/ext4/readpage.c
@@ -46,7 +46,6 @@
 #include <linux/cleancache.h>
 
 #include "ext4.h"
-#include <trace/events/android_fs.h>
 
 static inline bool ext4_bio_encrypted(struct bio *bio)
 {
@@ -90,9 +89,6 @@ static void mpage_end_io(struct bio *bio)
 	struct bio_vec *bv;
 	int i;
 
-	if (trace_android_fs_dataread_start_enabled())
-		ext4_trace_read_completion(bio);
-
 	if (ext4_bio_encrypted(bio)) {
 		if (bio->bi_status) {
 			fscrypt_release_ctx(bio->bi_private);
@@ -119,24 +115,6 @@ static void mpage_end_io(struct bio *bio)
 static void
 ext4_submit_bio_read(struct bio *bio)
 {
-	if (trace_android_fs_dataread_start_enabled()) {
-		struct page *first_page = bio->bi_io_vec[0].bv_page;
-
-		if (first_page != NULL) {
-			char *path, pathbuf[MAX_TRACE_PATHBUF_LEN];
-
-			path = android_fstrace_get_pathname(pathbuf,
-						    MAX_TRACE_PATHBUF_LEN,
-						    first_page->mapping->host);
-			trace_android_fs_dataread_start(
-				first_page->mapping->host,
-				page_offset(first_page),
-				bio->bi_iter.bi_size,
-				current->pid,
-				path,
-				current->comm);
-		}
-	}
 	submit_bio(bio);
 }
 

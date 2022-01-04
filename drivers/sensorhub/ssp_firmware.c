fa9b5608745c1a1fa0f21621d16d3120c68a0cb8
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2022-01-04 13:14:11 +0530

    sensorhub: ssp_firmware: use memcpy_toio() for download_sensorhub_firmware() Updated memcpy routines causes the loading of sensorhub firmware to fail.
    A similar change to upstream made the pcie endpoint functions switch to memcpy_toio(), as such we also switch to using memcpy_toio()
    
    Signed-off-by: Dark-Matter7232 <kerneldeveloper7232@gmail.com>

--- drivers/sensorhub/ssp_firmware.c
+++ drivers/sensorhub/ssp_firmware.c
@@ -221,7 +221,7 @@ int download_sensorhub_firmware(struct ssp_data *data, struct device *dev, void 
 
 	ssp_infof("fw type %d bin(size:%d) on %lx", data->fw_type, (int)fw_size, (unsigned long)addr);
 
-	memcpy(addr, fw_buf, fw_size);
+	memcpy_toio((void __iomem *)addr, fw_buf, fw_size);
 
 	if (entry)
 		release_firmware(entry);

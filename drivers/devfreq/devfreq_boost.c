c5ca4845eb053b21ba34ae41710043c925ee5ebb
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2022-03-01 11:21:59 +0530

    stune: Do not perform negative boosting
    Signed-off-by: Dark-Matter7232 <kerneldeveloper7232@gmail.com>

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -234,16 +234,14 @@ static int fb_notifier_cb(struct notifier_block *nb, unsigned long action,
 		if (*blank == FB_BLANK_UNBLANK) {
                         set_stune_boost("top-app", 2, &boost_slot);
                         set_stune_boost("foreground", 1, &boost_slot);
-                        set_stune_boost("background", -25, &boost_slot);
                         do_prefer_idle("top-app", 1);
                         
 			clear_bit(SCREEN_OFF, &b->state);
 			__devfreq_boost_kick_max(b,
 				CONFIG_DEVFREQ_WAKE_BOOST_DURATION_MS);
 		} else {
-                        set_stune_boost("top-app", -30, &boost_slot);
-                        set_stune_boost("foreground", -30, &boost_slot);
-                        set_stune_boost("background", -30, &boost_slot);
+                        set_stune_boost("top-app", 0, &boost_slot);
+                        set_stune_boost("foreground", 0, &boost_slot);
                         do_prefer_idle("top-app", 0);
   
 			set_bit(SCREEN_OFF, &b->state);

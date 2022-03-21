14fd46118937fc1d492d1095ae3feec32002793b
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2022-03-21 13:27:06 +0530

    Revert "mm: Omit RCU read lock in list_lru_count_one() when RCU isn't needed"
    This reverts commit e375935553a66cd3a47f11df4114c3eced865de7.

--- mm/list_lru.c
+++ mm/list_lru.c
@@ -161,7 +161,6 @@ EXPORT_SYMBOL_GPL(list_lru_isolate_move);
 unsigned long list_lru_count_one(struct list_lru *lru,
 				 int nid, struct mem_cgroup *memcg)
 {
-#if defined(CONFIG_MEMCG) && !defined(CONFIG_SLOB)
 	struct list_lru_node *nlru = &lru->node[nid];
 	struct list_lru_one *l;
 	unsigned long count;
@@ -172,9 +171,6 @@ unsigned long list_lru_count_one(struct list_lru *lru,
 	rcu_read_unlock();
 
 	return count;
-#else
-	return READ_ONCE(lru->node[nid].lru.nr_items);
-#endif
 }
 EXPORT_SYMBOL_GPL(list_lru_count_one);
 

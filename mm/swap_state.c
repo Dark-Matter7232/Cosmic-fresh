ca3c32ba6197ce3c459abfff1bc479109133f3d9
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2022-03-28 17:00:01 +0530

    Revert "mm: merge Samsung mm hacks" * Drop it for a cleaner merge
    This reverts commit adc585060d0d911202638de58eb51ef19dd17c9f.

--- mm/swap_state.c
+++ mm/swap_state.c
@@ -473,7 +473,6 @@ struct page *read_swap_cache_async(swp_entry_t entry, gfp_t gfp_mask,
 	return retpage;
 }
 
-#ifdef CONFIG_SWAP_ENABLE_READAHEAD
 static unsigned int __swapin_nr_pages(unsigned long prev_offset,
 				      unsigned long offset,
 				      int hits,
@@ -534,7 +533,6 @@ static unsigned long swapin_nr_pages(unsigned long offset)
 
 	return pages;
 }
-#endif
 
 /**
  * swapin_readahead - swap in pages in hope we need them soon
@@ -562,15 +560,13 @@ static unsigned long swapin_nr_pages(unsigned long offset)
 struct page *swapin_readahead(swp_entry_t entry, gfp_t gfp_mask,
 			struct vm_area_struct *vma, unsigned long addr)
 {
-	bool do_poll = true;
-#ifdef CONFIG_SWAP_ENABLE_READAHEAD
 	struct page *page;
 	unsigned long entry_offset = swp_offset(entry);
 	unsigned long offset = entry_offset;
 	unsigned long start_offset, end_offset;
 	unsigned long mask;
 	struct blk_plug plug;
-	bool page_allocated;
+	bool do_poll = true, page_allocated;
 
 	mask = swapin_nr_pages(offset) - 1;
 	if (!mask)
@@ -605,7 +601,6 @@ struct page *swapin_readahead(swp_entry_t entry, gfp_t gfp_mask,
 
 	lru_add_drain();	/* Push any new pages onto the LRU now */
 skip:
-#endif
 	return read_swap_cache_async(entry, gfp_mask, vma, addr, do_poll);
 }
 #else
@@ -651,7 +646,6 @@ void exit_swap_address_space(unsigned int type)
 	kvfree(spaces);
 }
 
-#ifdef CONFIG_SWAP_ENABLE_READAHEAD
 static inline void swap_ra_clamp_pfn(struct vm_area_struct *vma,
 				     unsigned long faddr,
 				     unsigned long lpfn,
@@ -732,16 +726,13 @@ struct page *swap_readahead_detect(struct vm_fault *vmf,
 
 	return NULL;
 }
-#endif
 
 struct page *do_swap_page_readahead(swp_entry_t fentry, gfp_t gfp_mask,
 				    struct vm_fault *vmf,
 				    struct vma_swap_readahead *swap_ra)
 {
-	struct vm_area_struct *vma = vmf->vma;
-	bool do_poll = true;
-#ifdef CONFIG_SWAP_ENABLE_READAHEAD
 	struct blk_plug plug;
+	struct vm_area_struct *vma = vmf->vma;
 	struct page *page;
 	pte_t *pte, pentry;
 	swp_entry_t entry;
@@ -750,8 +741,6 @@ struct page *do_swap_page_readahead(swp_entry_t fentry, gfp_t gfp_mask,
 
 	if (swap_ra->win == 1)
 		goto skip;
-
-	do_poll = false;
 
 	blk_start_plug(&plug);
 	for (i = 0, pte = swap_ra->ptes; i < swap_ra->nr_pte;
@@ -781,7 +770,6 @@ struct page *do_swap_page_readahead(swp_entry_t fentry, gfp_t gfp_mask,
 	blk_finish_plug(&plug);
 	lru_add_drain();
 skip:
-#endif
 	return read_swap_cache_async(fentry, gfp_mask, vma, vmf->address,
 				     swap_ra->win == 1);
 }

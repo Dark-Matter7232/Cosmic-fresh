386bd3b5f8446d8e08cd9babba9703c70140161b
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2022-04-16 16:05:15 +0530

    Revert "exynos-dm: Wrap exynos-ss debugging parts"
    This reverts commit 7eff63d98c33635debcbe7c0d687f8067014ff96.

--- drivers/soc/samsung/exynos-dm.c
+++ drivers/soc/samsung/exynos-dm.c
@@ -664,15 +664,12 @@ static bool max_flag = false;
 static int __policy_update_call_to_DM(int dm_type, u32 min_freq, u32 max_freq)
 {
 	struct exynos_dm_data *dm;
-#ifdef CONFIG_EXYNOS_SNAPSHOT_DM
 	struct timeval pre, before, after;
-#endif
 #ifdef CONFIG_EXYNOS_ACPM
 	struct ipc_config config;
 	unsigned int cmd[4];
 	int size, ch_num, ret;
 #endif
-#ifdef CONFIG_EXYNOS_SNAPSHOT_DM
 	s32 time = 0, pre_time = 0;
 
 #ifdef CONFIG_DEBUG_SNAPSHOT_DM
@@ -680,7 +677,6 @@ static int __policy_update_call_to_DM(int dm_type, u32 min_freq, u32 max_freq)
 #endif
 	do_gettimeofday(&pre);
 	do_gettimeofday(&before);
-#endif
 
 	min_freq = min(min_freq, max_freq);
 
@@ -717,7 +713,6 @@ static int __policy_update_call_to_DM(int dm_type, u32 min_freq, u32 max_freq)
 #endif
 
 out:
-#ifdef CONFIG_EXYNOS_SNAPSHOT_DM
 	do_gettimeofday(&after);
 
 	pre_time = (before.tv_sec - pre.tv_sec) * USEC_PER_SEC +
@@ -728,7 +723,7 @@ out:
 #ifdef CONFIG_DEBUG_SNAPSHOT_DM
 	dbg_snapshot_dm((int)dm_type, min_freq, max_freq, pre_time, time);
 #endif
-#endif
+
 	return 0;
 }
 
@@ -789,7 +784,6 @@ static int __DM_CALL(int dm_type, unsigned long *target_freq)
 	int ret;
 	unsigned int relation = EXYNOS_DM_RELATION_L;
 	u32 old_min_freq;
-#ifdef CONFIG_EXYNOS_SNAPSHOT_DM
 	struct timeval pre, before, after;
 	s32 time = 0, pre_time = 0;
 
@@ -798,7 +792,7 @@ static int __DM_CALL(int dm_type, unsigned long *target_freq)
 #endif
 	do_gettimeofday(&pre);
 	do_gettimeofday(&before);
-#endif
+
 	dm = &exynos_dm->dm_data[dm_type];
 	old_min_freq = dm->min_freq;
 	dm->gov_min_freq = (u32)(*target_freq);
@@ -850,7 +844,6 @@ static int __DM_CALL(int dm_type, unsigned long *target_freq)
 		max_order[i] = DM_EMPTY;
 	}
 
-#ifdef CONFIG_EXYNOS_SNAPSHOT_DM
 	do_gettimeofday(&after);
 
 	pre_time = (before.tv_sec - pre.tv_sec) * USEC_PER_SEC +
@@ -861,7 +854,7 @@ static int __DM_CALL(int dm_type, unsigned long *target_freq)
 #ifdef CONFIG_DEBUG_SNAPSHOT_DM
 	dbg_snapshot_dm((int)dm_type, *target_freq, 3, pre_time, time);
 #endif
-#endif
+
 	return 0;
 }
 

b0f3b46898151ab7c791ffedd2b6654cd696508f
tytydraco <tylernij@gmail.com>
2022-05-14 11:30:10 +0530

    sched: fair: Use shorter scheduling periods for lower latency Reduce the maximum scheduling period for lower latency.
    Co-authored-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>
    Signed-off-by: Dark-Matter7232 <kerneldeveloper7232@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -60,8 +60,8 @@
  *
  * (default: 6ms * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_latency			= 6000000ULL;
-unsigned int normalized_sysctl_sched_latency		= 6000000ULL;
+unsigned int sysctl_sched_latency			= 5000000ULL;
+unsigned int normalized_sysctl_sched_latency		= 5000000ULL;
 
 /*
  * Enable/disable honoring sync flag in energy-aware wakeups.
@@ -88,10 +88,10 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_L
 /*
  * Minimal preemption granularity for CPU-bound tasks:
  *
- * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
+ * (default: 0.625 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity		= 750000ULL;
-unsigned int normalized_sysctl_sched_min_granularity	= 750000ULL;
+unsigned int sysctl_sched_min_granularity		= 625000ULL;
+unsigned int normalized_sysctl_sched_min_granularity	= 625000ULL;
 
 /*
  * This value is kept at sysctl_sched_latency/sysctl_sched_min_granularity

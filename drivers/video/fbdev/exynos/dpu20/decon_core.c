10416434dd45625a1063199781f278dd8d3ee104
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2022-05-15 23:00:36 +0530

    drivers/video: fbdev/dpu20: reduce devfreq boost duration
    Signed-off-by: Dark-Matter7232 <kerneldeveloper7232@gmail.com>

--- drivers/video/fbdev/exynos/dpu20/decon_core.c
+++ drivers/video/fbdev/exynos/dpu20/decon_core.c
@@ -2720,7 +2720,7 @@ static int decon_set_win_config(struct decon_device *decon,
 
 	num_of_window = decon_get_active_win_count(decon, win_data);
 	if (num_of_window) {
-	         devfreq_boost_kick_max(DEVFREQ_EXYNOS_MIF, 100);
+	         devfreq_boost_kick_max(DEVFREQ_EXYNOS_MIF, 70);
 		win_data->retire_fence = decon_create_fence(decon, &sync_file);
 		if (win_data->retire_fence < 0)
 			goto err_prepare;
@@ -2746,7 +2746,7 @@ static int decon_set_win_config(struct decon_device *decon,
 			sizeof(struct decon_rect));
 
 	if (num_of_window) {
-		devfreq_boost_kick_max(DEVFREQ_EXYNOS_MIF, 100);
+		devfreq_boost_kick_max(DEVFREQ_EXYNOS_MIF, 70);
 		decon_create_release_fences(decon, win_data, sync_file);
 #if !defined(CONFIG_SUPPORT_LEGACY_FENCE)
 		regs->retire_fence = dma_fence_get(sync_file->fence);
